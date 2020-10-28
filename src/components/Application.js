7a085784fdd80486686d32f7fcb70bf5cf6759e3
skendanavian <s.nissen.bass@gmail.com>
2020-10-28 16:44:47 -0600

    added useState with other states inside object and changed prop names accordingly

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,12 +56,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {},
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("api/days").then((res) => {
-      setDays([...res.data]);
+      setDays(res.data);
     });
   }, []);
 
@@ -78,7 +84,7 @@ export default function Application(props) {
             />
             <hr className="sidebar__separator sidebar--centered" />
             <nav className="sidebar__menu">
-              <DayList days={days} day={day} setDay={setDay} />
+              <DayList days={state.days} day={state.day} setDay={setDay} />
             </nav>
             <img
               className="sidebar__lhl sidebar--centered"
